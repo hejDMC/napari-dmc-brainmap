272cbc65b3f3b4ffd2d317d307c3aaf187f8f9b6
home workstation <jung.neurosc>
2023-04-26 12:44:26 +0200

    single channels for segmentation

--- src/napari_dmc_brainmap/segment/segment.py
+++ src/napari_dmc_brainmap/segment/segment.py
@@ -15,10 +15,22 @@ def cmap_cells():
     cmap = {
         'dapi': 'yellow',
         'green': 'magenta',
+        'n3': 'gray',
         'cy3': 'cyan',
-        # 'cy5': 'lightblue'
+        'cy5': 'lightblue'
     }
     return cmap
+
+def cmap_display():
+    cmap = {
+        'dapi': 'blue',
+        'green': 'green',
+        'n3': 'orange',
+        'cy3': 'red',
+        'cy5': 'pink'
+    }
+    return cmap
+
 
 
 def default_save_dict():
@@ -35,19 +47,20 @@ def default_save_dict():
     input_path=dict(widget_type='FileEdit', label='input path (animal_id): ', mode='d',
                     tooltip='directory of folder containing subfolders with e.g. images, segmentation results, NOT '
                                 'folder containing segmentation results'),
+    single_channel_bool=dict(widget_type='CheckBox', text='use single channel', value=False,
+                             tooltip='tick to use single channel images (not RGB), one can still select '
+                                     'multiple channels'),
     seg_type=dict(widget_type='ComboBox', label='segmentation type',
                     choices=['cells', 'injection_side'], value='cells',
                     tooltip='select to either segment cells (points) or areas (e.g. for the injection side)'
                             'IMPORTANT: before switching between types, load next image, delete all image layers'
                             'and reload image of interest!'),
     channels=dict(widget_type='Select', label='selected channels', value=['green', 'cy3'],
-                      choices=['dapi', 'green', 'cy3'],
+                      choices=['dapi', 'green', 'n3', 'cy3', 'cy5'],
                       tooltip='select channels to be selected for cell segmentation, '
                               'to select multiple hold ctrl/shift'),
     image_idx=dict(widget_type='LineEdit', label='image to be loaded', value=0,
                     tooltip='index (int) of image to be loaded and segmented next'),
-    # load_dapi_bool=dict(widget_type='CheckBox', text='load blue channel', value=False,
-    #                     tooltip='option to load blue channel (0: cy3; 1: green; 2: dapi)'),
     call_button=False
 )
 def segment_widget(
@@ -56,7 +69,7 @@ def segment_widget(
     seg_type,
     channels,
     image_idx,
-    # load_dapi_bool
+    single_channel_bool
 ) -> None:
 
     return segment_widget
@@ -88,7 +101,7 @@ class SegmentWidget(QWidget):
         image_idx = int(segment_widget.image_idx.value)
         seg_type = segment_widget.seg_type.value
         channels = segment_widget.channels.value
-        seg_im_dir, seg_im_list, seg_im_suffix = get_info(input_path, 'rgb')
+
         if len(self.viewer.layers) == 0:  # no open images, set save_dict to defaults
             self.save_dict = default_save_dict()
         if type(self.save_dict['image_idx']) == int:  # todo there must be a better way :-D (for image_idx = 0)
@@ -96,15 +109,25 @@ class SegmentWidget(QWidget):
         del (self.viewer.layers[:])  # remove open layers
 
         try:
-            im = natsorted([f.parts[-1] for f in seg_im_dir.glob('*.tif')])[
-                image_idx]  # this detour due to some weird bug, list of paths was only sorted, not natsorted
-            path_to_im = seg_im_dir.joinpath(im)
-            self._load_next(path_to_im, seg_type, channels, image_idx)
+            if not segment_widget.single_channel_bool.value:
+                seg_im_dir, seg_im_list, seg_im_suffix = get_info(input_path, 'rgb')
+                im = natsorted([f.parts[-1] for f in seg_im_dir.glob('*.tif')])[
+                    image_idx]  # this detour due to some weird bug, list of paths was only sorted, not natsorted
+                path_to_im = seg_im_dir.joinpath(im)
+                self._load_next_rgb(path_to_im, seg_type, channels, image_idx)
+            else:
+                for chan in channels:
+                    seg_im_dir, seg_im_list, seg_im_suffix = get_info(input_path, 'single_channel', channel=chan)
+                    im = natsorted([f.parts[-1] for f in seg_im_dir.glob('*.tif')])[
+                        image_idx]  # this detour due to some weird bug, list of paths was only sorted, not natsorted
+                    path_to_im = seg_im_dir.joinpath(im)
+                    self._load_next_single(path_to_im, seg_type, chan, image_idx)
+
         except IndexError:
             print("Index out of range, check that index matches image count in " + str(seg_im_dir))
 
 
-    def _load_next(self, path_to_im, seg_type, channels, image_idx):
+    def _load_next_rgb(self, path_to_im, seg_type, channels, image_idx):
         self.save_dict = self._update_save_dict(image_idx, seg_type)
         im_loaded = cv2.imread(str(path_to_im))  # loads RGB as BGR
         if 'cy3' in channels:
@@ -122,6 +145,23 @@ class SegmentWidget(QWidget):
             cmap_dict = cmap_cells()
             for chan in channels:
                 self.viewer.add_points(size=5, name=chan, face_color=cmap_dict[chan])
+        print("loaded " + path_to_im.parts[-1] + " (cnt=" + str(image_idx) + ")")
+        image_idx += 1
+        change_index(image_idx)
+
+    def _load_next_single(self, path_to_im, seg_type, chan, image_idx):
+        self.save_dict = self._update_save_dict(image_idx, seg_type)
+        cmap_disp = cmap_display()
+        im_loaded = cv2.imread(str(path_to_im), cv2.IMREAD_GRAYSCALE)
+        self.viewer.add_image(im_loaded, name=chan + ' channel', colormap=cmap_disp[chan], opacity=0.5)
+        self.viewer.layers[chan + ' channel'].contrast_limits = [0, 100]
+
+        if seg_type == 'injection_side':
+            if 'injection' not in self.viewer.layers:
+                self.viewer.add_shapes(name='injection', face_color='purple', opacity=0.4)
+        elif seg_type == 'cells':  # todo presegment for cells
+            cmap_dict = cmap_cells()
+            self.viewer.add_points(size=5, name=chan, face_color=cmap_dict[chan])
         print("loaded " + path_to_im.parts[-1] + " (cnt=" + str(image_idx) + ")")
         image_idx += 1
         change_index(image_idx)
