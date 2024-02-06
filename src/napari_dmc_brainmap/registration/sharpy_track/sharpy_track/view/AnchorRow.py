c9865dbc84b53f9e3d0bb3f6c8a80bd55fc58956
Xiao <caoxiao@live.com>
2024-02-06 10:37:24 +0100

    interpolate position tab outline

---
+++ src/napari_dmc_brainmap/registration/sharpy_track/sharpy_track/view/AnchorRow.py
@@ -0,0 +1,60 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QWidget, QHBoxLayout, QSpinBox,QLabel,QPushButton,QDoubleSpinBox
+from napari_dmc_brainmap.utils import coord_mm_transform
+import numpy as np
+
+class AnchorRow(QWidget):
+    def __init__(self, regHelper):
+        super().__init__()
+        self.regHelper = regHelper
+        # create horizontal layout
+        self.anc_hbox = QHBoxLayout()
+        self.setLayout(self.anc_hbox)
+        regHelper.anchor_vbox.addWidget(self)
+        # add widgets to horizontal layout
+        self.spinSliceIndex = QSpinBox()
+        self.spinSliceIndex.setValue(regHelper.regViewer.status.currentSliceNumber)
+        self.spinSliceIndex.setMinimum(0)
+        self.spinSliceIndex.setMaximum(regHelper.regViewer.status.sliceNum - 1)
+        self.spinSliceIndex.setSingleStep(1)
+        self.spinSliceIndex.valueChanged.connect(self.slice_index_changed)
+
+        self.anc_hbox.addWidget(self.spinSliceIndex)
+
+        self.sliceNameLabel = QLabel()
+        self.sliceNameLabel.setText(regHelper.regViewer.status.imgNameDict[
+            regHelper.regViewer.status.currentSliceNumber])
+        self.anc_hbox.addWidget(self.sliceNameLabel)
+
+        self.spinAPmm = QDoubleSpinBox()
+        self.spinAPmm.setDecimals(2)
+        self.spinAPmm.setValue(regHelper.regViewer.status.current_z)
+        self.spinAPmm.setMinimum(-7.80)
+        self.spinAPmm.setMaximum(5.40)
+        self.spinAPmm.setSingleStep(0.01)
+        self.spinAPmm.setPrefix("AP: ")
+        self.spinAPmm.setSuffix(" mm")
+        self.spinAPmm.valueChanged.connect(self.ap_mm_changed)
+
+        self.anc_hbox.addWidget(self.spinAPmm)
+
+
+        self.trash_btn = QPushButton("Trash")
+        self.trash_btn.clicked.connect(self.trash_action)
+        self.anc_hbox.addWidget(self.trash_btn)
+    
+    def trash_action(self):
+        # remove self from anchor_vbox layout
+        self.regHelper.anchor_vbox.removeWidget(self)
+
+
+    def slice_index_changed(self):
+        self.regHelper.regViewer.widget.sampleSlider.setValue(self.spinSliceIndex.value())
+        self.sliceNameLabel.setText(self.regHelper.regViewer.status.imgNameDict[
+            self.regHelper.regViewer.status.currentSliceNumber])
+        self.spinAPmm.setValue(self.regHelper.regViewer.status.current_z)
+    
+    def ap_mm_changed(self):
+        self.regHelper.regViewer.status.current_z = np.round(self.spinAPmm.value(),2)
+        self.regHelper.regViewer.widget.viewerLeft.loadSlice(self.regHelper.regViewer)
+        
--- src/napari_dmc_brainmap/registration/sharpy_track/sharpy_track/view/Tools.py
+++ src/napari_dmc_brainmap/registration/sharpy_track/sharpy_track/view/Tools.py
@@ -1,5 +1,7 @@
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QWidget,QStackedLayout,QPushButton,QVBoxLayout,QHBoxLayout,QLabel,QMainWindow
+from PyQt5.QtWidgets import QWidget,QStackedLayout,QPushButton,QVBoxLayout,QHBoxLayout,QLabel,QMainWindow,QComboBox
+from PyQt5 import QtGui
+from napari_dmc_brainmap.registration.sharpy_track.sharpy_track.view.AnchorRow import AnchorRow
 
 class RegistrationHelper(QMainWindow):
     def __init__(self, regViewer) -> None:
@@ -20,19 +22,63 @@ class RegistrationHelper(QMainWindow):
         self.btn_ip = QPushButton("Interpolate Position")
         self.btn_re = QPushButton("Registration Editor")
 
-        self.btn_ip.pressed.connect(self.activate_ipPage)
-        self.btn_re.pressed.connect(self.activate_rePage)
+        self.btn_ip.clicked.connect(self.activate_ipPage)
+        self.btn_re.clicked.connect(self.activate_rePage)
 
         self.buttonlayout.addWidget(self.btn_ip)
         self.buttonlayout.addWidget(self.btn_re)
-        # tab contents
-        self.label_ip = QLabel()
-        
-        self.label_ip.setText("interpolate")
+        # tab structure: Interpolate Position
+        self.ip_widget = QWidget()
+        self.ip_vbox = QVBoxLayout()
+        # self.ip_vbox.addStretch() # align to the top
+        self.ip_widget.setLayout(self.ip_vbox)
+        self.stacklayout.addWidget(self.ip_widget) # only addChildLayout under stacklayout
+        # location plot hbox
+        self.locplot_hbox = QHBoxLayout()
+        self.ip_vbox.addLayout(self.locplot_hbox)
+        # preview button and add button vbox
+        self.previewadd_vbox = QVBoxLayout()
+        self.locplot_hbox.addLayout(self.previewadd_vbox)
+            # preview button
+        self.preview_btn = QPushButton("Preview")
+        self.previewadd_vbox.addWidget(self.preview_btn)
+            # add button
+        self.add_btn = QPushButton("Add")
+        self.add_btn.clicked.connect(self.add_action)
+        self.previewadd_vbox.addWidget(self.add_btn)
+            # section location illustration
+        self.preview_label = QLabel()
+        # insert demo image, replace with real time rendering in the future
+        demo_img_path = "C:\\Users\\xiao\\GitHub\\napari-dmc-brainmap\\src\\napari_dmc_brainmap\\registration\\sharpy_track\\sharpy_track\\images\\locationplot.png" 
+        with open(demo_img_path,'rb') as f:
+            demo_img_bin = f.read()
+        demo_img = QtGui.QImage()
+        demo_img.loadFromData(demo_img_bin)
+        self.preview_label.setPixmap(QtGui.QPixmap.fromImage(demo_img))
+        # self.preview_label.setFixedSize(750,300)
+
+        self.locplot_hbox.addWidget(self.preview_label)
+            # abort and apply buttons in a vbox
+        self.abort_apply_vbox = QVBoxLayout()
+        self.locplot_hbox.addLayout(self.abort_apply_vbox)
+        self.abort_btn = QPushButton("Abort")
+        self.apply_btn = QPushButton("Apply")
+        self.abort_apply_vbox.addWidget(self.abort_btn)
+        self.abort_apply_vbox.addWidget(self.apply_btn)
+
+        # anchor widget
+        self.anchor_widget = QWidget()
+        self.anchor_vbox = QVBoxLayout()
+        self.anchor_widget.setLayout(self.anchor_vbox)
+        self.ip_vbox.addWidget(self.anchor_widget)
+
+
+
+        # tab structure: Registration Editor
         self.label_re = QLabel()
         self.label_re.setText("editor")
-        self.stacklayout.addWidget(self.label_ip)
         self.stacklayout.addWidget(self.label_re)
+        # set default display Interpolate Position tab
         self.stacklayout.setCurrentIndex(0)
 
     def activate_ipPage(self):
@@ -40,6 +86,15 @@ class RegistrationHelper(QMainWindow):
     
     def activate_rePage(self):
         self.stacklayout.setCurrentIndex(1)
+    
+    def add_action(self):
+        # create anchor object
+        AnchorRow(self)
+
+
+
+
+
 
 
     
