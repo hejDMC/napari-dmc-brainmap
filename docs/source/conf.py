d99a150217d3b03160d0160178d73a1315f54ade
felix-office <jung.neurosc@gmail.com>
2025-08-13 13:55:15 +0200

    0.1.7b5 release

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 project = 'napari-dmc-brainmap'
 copyright = '2025, DMC'
 author = 'DMC'
-release = '0.1.7b4'
+release = '0.1.7b5'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
