2e588cafebf67e564c34c78d22418fe537c697df
Xiao <caoxiao@live.com>
2025-08-29 17:20:33 +0200

    halfway implementing load record

--- src/napari_dmc_brainmap/registration/sharpy_track/sharpy_track/model/MeasurementHandler.py
+++ src/napari_dmc_brainmap/registration/sharpy_track/sharpy_track/model/MeasurementHandler.py
@@ -1,4 +1,7 @@
 import json
+import numpy as np
+from src.napari_dmc_brainmap.registration.sharpy_track.sharpy_track.model.calculation import fitGeoTrans, mapPointTransform
+from src.napari_dmc_brainmap.registration.sharpy_track.sharpy_track.view.TreRow import TreRow
 
 class MeasurementHandler:
     def __init__(self, measurementPage) -> None:
@@ -29,9 +32,48 @@ class MeasurementHandler:
     
     def load_measurement_record(self):
         # check if current sample has measurement record
-        #TODO: load measurement record from json
-        # if self.regViewer.status.currentSliceNumber in self.
-        return
+        if str(self.regViewer.status.currentSliceNumber) in self.json_data["imgName"]:
+            self.paint_rows()
+        else:
+            pass
+
+    def paint_rows(self):
+        imgIndex = self.regViewer.status.currentSliceNumber
+        # set imgIndex
+        self.regViewer.measurementPage.active_rows["imgIndex"] = imgIndex
+        for source_xy in self.json_data["sourceDots"][imgIndex]:
+            self.regViewer.measurementPage.active_rows["source_coords"].append(source_xy)
+            # recalculate tranformation matrix from registration json
+            tform = fitGeoTrans(self.regViewer.status.sampleDots[imgIndex], 
+                                self.regViewer.status.atlasDots[imgIndex])
+            self.regViewer.measurementPage.active_rows["tform_matrix"] = tform.tolist()
+            # remap target coordinates
+            target_xy = mapPointTransform(source_xy[0], source_xy[1], tform)
+            target_xy = [np.round(target_xy[0]).astype(int), 
+                         np.round(target_xy[1]).astype(int)]
+            self.regViewer.measurementPage.active_rows["target_coords"].append(target_xy)
+
+        for truth_xy in self.json_data["truthDots"][imgIndex]:
+            self.regViewer.measurementPage.active_rows["truth_coords"].append(truth_xy)
+        # calculate tre score
+        for tru,tar in zip(self.regViewer.measurementPage.active_rows["truth_coords"],
+                           self.regViewer.measurementPage.active_rows["target_coords"]):
+            TRE = np.sqrt((tru[0] - tar[0])**2 + (tru[1] - tar[1])**2)
+            self.regViewer.measurementPage.active_rows["tre_score"].append(np.round(TRE, 4).astype(str))
+        # create row object (remove_btn disabled)
+        for row_i in range(len(self.regViewer.measurementPage.active_rows["tre_score"])):
+            row_obj = TreRow(self.regViewer.measurementPage)
+            row_obj.source_pos_label.setText(f"({self.regViewer.measurementPage.active_rows['source_coords'][row_i][0]}, {self.regViewer.measurementPage.active_rows['source_coords'][row_i][1]})")
+            row_obj.target_pos_label.setText(f"({self.regViewer.measurementPage.active_rows['target_coords'][row_i][0]}, {self.regViewer.measurementPage.active_rows['target_coords'][row_i][1]})")
+            row_obj.true_pos_label.setText(f"({self.regViewer.measurementPage.active_rows['truth_coords'][row_i][0]}, {self.regViewer.measurementPage.active_rows['truth_coords'][row_i][1]})")
+            row_obj.tre_score_label.setText(f"{float(self.regViewer.measurementPage.active_rows["tre_score"][row_i]):.2f}")
+        # create dot objects
+        self.regViewer.widget.viewerRight.addSourceDot()
+        self.regViewer.widget.viewerLeft.addTruthDot()
+
+        # enable remove_btn
+
+
 
     def save_measurement_record(self):
         # check if current sample has measurement record
